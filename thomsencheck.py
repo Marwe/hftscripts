6542f1975ba9b5c4fed9fbee74fab14a0723285c
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-12-10 22:32:23 +0100

    fixed default parameters "file" ('append' appends to default), stdin encoding issue, TODO: if output is redirected, the print statements bail

--- thomsencheck.py
+++ thomsencheck.py
@@ -8,16 +8,23 @@ import sys
 import os
 import gzip
 
+defaultfiles=[]
+defaultfiles.append(os.path.join(os.path.dirname(__file__),'journallist_thomsen.txt.gz'))
+defaultfiles.append(os.path.join(os.path.dirname(__file__),'journallist_thum.txt'))
+
+
 parser = argparse.ArgumentParser(description='check if a line appears in a file, fuzzy by Levenshtein distance (all lowercase comparison)')
 parser.add_argument(metavar='L', type=str, nargs='*', dest='line', default=None,
                    help='line to look for, if empty use stdin')
 parser.add_argument('-d', '--distance', type=int, dest='distance', default=4,
                    help='max Levenshtein distance')
-parser.add_argument('-f', '--file', action='append', default=[os.path.join(os.path.dirname(__file__),'journallist_thomsen.txt.gz'), os.path.join(os.path.dirname(__file__),'journallist_thum.txt')], dest='file',
-                   help='files to be processed, gzipped files are handled')
+parser.add_argument('-f', '--file', action='append', dest='file',
+                   help='files to be processed, gzipped files are handled. If not given, defaults to '+str(defaultfiles))
 
 args = parser.parse_args()
 #print args
+if args.file is None:
+    args.file=defaultfiles
 
 tlists={}
 for filename in args.file:
@@ -39,17 +46,19 @@ for filename in args.file:
 for bbs in args.line:
     for f,goodlist in tlists.items():
         for g in goodlist:
-            b=bbs.decode(sys.stdin.encoding)
+            b=bbs.decode(sys.stdin.encoding if sys.stdin.encoding else 'UTF-8')
             if Levenshtein.distance(g.lower().strip(),unicode(b).lower().strip()) < args.distance:
-                print "%s similar to %s in %s" % (g, b,f)
+                print u"%s similar to %s in %s" % (g, b,f)
 
 if len(args.line) == 0:
     while True:
         bbs = sys.stdin.readline()
         if not bbs:
             break
+        if (len(bbs) < 2) or (bbs is None):
+            continue
         for f,goodlist in tlists.items():
             for g in goodlist:
-                b=bbs.decode(sys.stdin.encoding)
+                b=bbs.decode(sys.stdin.encoding if sys.stdin.encoding else 'UTF-8')
                 if Levenshtein.distance(g.lower().strip(),unicode(b).lower().strip()) < args.distance:
-                    print "%s similar to %s in %s" % (g, b,f)
+                    print u"%s similar to %s in %s" % (g, b,f)
