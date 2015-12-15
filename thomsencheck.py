157a5e83c13204bb832bf34ba7ca845d1a3d6651
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-12-15 11:42:01 +0100

    thomsencheck with yieldfct

--- thomsencheck.py
+++ thomsencheck.py
@@ -7,6 +7,7 @@ import argparse
 import sys
 import os
 import gzip
+import re
 
 defaultfiles=[]
 defaultfiles.append(os.path.join(os.path.dirname(__file__),'journallist_thomsen.txt.gz'))
@@ -18,6 +19,8 @@ parser.add_argument(metavar='L', type=str, nargs='*', dest='line', default=None,
                    help='line to look for, if empty use stdin')
 parser.add_argument('-d', '--distance', type=int, dest='distance', default=4,
                    help='max Levenshtein distance')
+parser.add_argument('-r', '--regex', action="store_true", dest='regex',
+                   help='use input lines as regex')
 parser.add_argument('-f', '--file', action='append', dest='file',
                    help='files to be processed, gzipped files are handled. If not given, defaults to '+str(defaultfiles))
 
@@ -35,30 +38,49 @@ for filename in args.file:
             with gzip.open(filename) as f:
                 #cgr=codecs.getreader('zlib')
                 fd = codecs.getreader("utf-8")(f)
-                tlists[filename] = fd.readlines()
+                tlists[filename] = fd.read().splitlines()
         else:
             with codecs.open(filename, "r", "utf8") as f:
-                tlists[filename]=f.readlines()
+                tlists[filename]=f.read().splitlines()
     except: # Error as e:
         print "error with "+filename
         continue
 
-for bbs in args.line:
+def yieldlistorstdin(alist=[]):
+    if len(alist)==0:
+        while True:
+            x=sys.stdin.readline()
+            if not x:
+                break
+            if (len(x) < 2) or (x is None):
+                continue
+            yield x.strip()
+    else:
+        for x in alist:
+            yield x
+
+for bbs in yieldlistorstdin(args.line):
+    if args.regex:
+        rx=re.compile('.*'+bbs+'.*', flags=re.IGNORECASE)
     for f,goodlist in tlists.items():
         for g in goodlist:
             b=bbs.decode(sys.stdin.encoding if sys.stdin.encoding else 'UTF-8')
             if Levenshtein.distance(g.lower().strip(),unicode(b).lower().strip()) < args.distance:
-                print u"%s similar to %s in %s" % (g, b,f)
-
-if len(args.line) == 0:
-    while True:
-        bbs = sys.stdin.readline()
-        if not bbs:
-            break
-        if (len(bbs) < 2) or (bbs is None):
-            continue
-        for f,goodlist in tlists.items():
-            for g in goodlist:
-                b=bbs.decode(sys.stdin.encoding if sys.stdin.encoding else 'UTF-8')
-                if Levenshtein.distance(g.lower().strip(),unicode(b).lower().strip()) < args.distance:
-                    print u"%s similar to %s in %s" % (g, b,f)
+                print u'similarity of "%s" to "%s" in "%s' % (b, g, f)
+            if args.regex:
+                m=rx.match(g)
+                if m is not None:
+                    print u'regex match of "%s": %s in %s' % (b, g, f)
+                    
+#if len(args.line) == 0:
+    #while True:
+        #bbs = sys.stdin.readline()
+        #if not bbs:
+            #break
+        #if (len(bbs) < 2) or (bbs is None):
+            #continue
+        #for f,goodlist in tlists.items():
+            #for g in goodlist:
+                #b=bbs.decode(sys.stdin.encoding if sys.stdin.encoding else 'UTF-8')
+                #if Levenshtein.distance(g.lower().strip(),unicode(b).lower().strip()) < args.distance:
+                    #print u'similarity of "%s" to "%s" in "%s' % (b, g, f)
